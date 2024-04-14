444f5c5d188499cb645039bfabbfd50e11ad53c4
ludon5681 <84600299+ludon5681@users.noreply.github.com>
2024-04-13 22:27:44 -0400

    Made "under construction" page

---
+++ index/script.js
@@ -0,0 +1,7 @@
+document.getElementById("next").addEventListener("click", () => {
+    document.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+})
+
+document.getElementById("prev").addEventListener("click", () => {
+    document.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+})
